a35cd54e5b1c35fbb82d22b0e02f3e333b88d941
chaaerim <cofla411@ewhain.net>
2023-02-07 05:22:40 +0900

    Greedy: BJ11399.js

---
+++ Greedy/BJ11399/BJ11399.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const [n, input] = fs.readFileSync("./input.txt").toString().trim().split("\n");
+
+solution(input)
+function solution(input){
+    const arr=input.split(' ')
+    console.log(arr)
+    arr.sort((a, b)=>{
+    return a-b})
+    let sum=0
+    for (let i=0;i<arr.length;i++){
+        for (let j=0;j<=i;j++){
+            sum+=Number(arr[j])
+        }
+    }
+    console.log(sum)
+}
+
+// 더 간단히 풀기
+// n은 개수 
+
+const answer = input
+  .split(" ")
+  .sort((a, b) => a - b)
+  .reduce((acc, cur, i) => 
+  acc + cur * (n - i), 0);
+
+console.log('!!!', answer);
