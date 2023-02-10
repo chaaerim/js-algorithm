8e1d6d761de053dbeeffa98e1ace03ed58bc16c0
chaaerim <cofla411@ewhain.net>
2023-02-10 16:08:04 +0900

    구현: BJ1316

---
+++ 구현/BJ1316/BJ1316.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const inputCount=Number(input.shift())
+let answer=0;
+
+
+const caseCount = Number(input[0]);
+let countGroupWord = 0;
+
+for (let i = 1; i <= caseCount; i++) {
+  const word = input[i];
+  const letter = [];
+  let isGroupWord = true;
+
+  for (let j = 0; j < word.length; j++) {
+    if (letter.indexOf(word[j]) === -1) {
+      letter.push(word[j]);
+    } else {
+      if (letter.indexOf(word[j]) !== letter.length - 1) {
+        isGroupWord = false;
+        break;
+      }
+    }
+  }
+
+  if (isGroupWord) {
+    countGroupWord += 1;
+  }
+}
+
+console.log(countGroupWord);
